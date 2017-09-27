c2da70bbe7a3ff12bf6a206bc9d216fd8ab7945d
MamakovAV <MamakovAV@W12MAMAKOVAV>
2017-09-27 09:10:21 +0300

    first

--- dotNet-lab3/Form1.Designer.cs
+++ dotNet-lab3/Form1.Designer.cs
@@ -28,12 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxMN = new System.Windows.Forms.TextBox();
+            this.textBoxFN = new System.Windows.Forms.TextBox();
+            this.textBoxLN = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.maskedTextBoxPhone = new System.Windows.Forms.MaskedTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.maskedTextBoxPhone);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBoxLN);
+            this.groupBox1.Controls.Add(this.textBoxMN);
+            this.groupBox1.Controls.Add(this.textBoxFN);
+            this.groupBox1.Location = new System.Drawing.Point(54, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(343, 366);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Студент";
+            // 
+            // textBoxMN
+            // 
+            this.textBoxMN.Location = new System.Drawing.Point(36, 100);
+            this.textBoxMN.Name = "textBoxMN";
+            this.textBoxMN.Size = new System.Drawing.Size(141, 20);
+            this.textBoxMN.TabIndex = 4;
+            // 
+            // textBoxFN
+            // 
+            this.textBoxFN.Location = new System.Drawing.Point(36, 45);
+            this.textBoxFN.Name = "textBoxFN";
+            this.textBoxFN.Size = new System.Drawing.Size(141, 20);
+            this.textBoxFN.TabIndex = 3;
+            // 
+            // textBoxLN
+            // 
+            this.textBoxLN.Location = new System.Drawing.Point(36, 155);
+            this.textBoxLN.Name = "textBoxLN";
+            this.textBoxLN.Size = new System.Drawing.Size(141, 20);
+            this.textBoxLN.TabIndex = 5;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(90, 496);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 319);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Имя:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Отчество:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Фамилия:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(36, 209);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(267, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Адрес:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 251);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Телефон:";
+            // 
+            // maskedTextBoxPhone
+            // 
+            this.maskedTextBoxPhone.Location = new System.Drawing.Point(36, 267);
+            this.maskedTextBoxPhone.Mask = "+0(000) 000-0000";
+            this.maskedTextBoxPhone.Name = "maskedTextBoxPhone";
+            this.maskedTextBoxPhone.Size = new System.Drawing.Size(112, 20);
+            this.maskedTextBoxPhone.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 604);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxLN;
+        private System.Windows.Forms.TextBox textBoxMN;
+        private System.Windows.Forms.TextBox textBoxFN;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxPhone;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- dotNet-lab3/Program.cs
+++ dotNet-lab3/Program.cs
@@ -4,13 +4,58 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// Вариант 9	
+//1.	Студенты(фамилия, имя, отчество, адрес, телефон).
+//2.	Предметы(название, объем лекций, объем практик, объем лабораторных работ).
+//3.	Учебный план(студент, предмет, оценка).
+//4.	Определение факультативов для студентов
+
 namespace dotNet_lab3
 {
+    using StudentCourse = Tuple<Student, Course>;
+    using StudentCourse2 = KeyValuePair<Student, Course>;
+
+    public class Student
+    {
+        public string FirsName;
+        public string MiddleName;
+        public string LastName;
+        public string Address;
+        public string Phone;
+    }
+
+    public class Course
+    {
+        public string Name;
+        public int LecturesCount;
+        public int PracticalCount;
+        public int LaborCount;
+    }
+
+    public class StudyPlan
+    {
+        Dictionary<StudentCourse, int> rating;
+        Dictionary<StudentCourse2, int> rating2;
+        void ddd()
+        {
+            Student st1 = new Student();
+            Course c1 = new Course();
+            rating.Add(new StudentCourse(st1, c1), 3);
+        }
+    }
+
+    public class StudyElective
+    {
+
+    }
+
+
+
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
+        static List<Student> students;
+        static List<Course> courses;
+
         [STAThread]
         static void Main()
         {
